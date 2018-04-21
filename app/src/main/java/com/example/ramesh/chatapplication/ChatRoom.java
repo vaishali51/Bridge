a57491559e9a2ec432aa0d911febc8a338f8b53b
vaishali51 <vaishalig714@gmail.com>
2018-04-21 22:08:01 +0530

    Final Update

--- app/src/main/java/com/example/ramesh/chatapplication/ChatRoom.java
+++ app/src/main/java/com/example/ramesh/chatapplication/ChatRoom.java
@@ -1,6 +1,7 @@
 package com.example.ramesh.chatapplication;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -8,13 +9,19 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,8 +32,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -37,15 +57,13 @@ public class ChatRoom extends AppCompatActivity {
 
     private FirebaseListAdapter<ChatMessage> adapter;
     SharedPreferences sharedPreferences;
+    public RelativeLayout chtbx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
         sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("language", "English");
-        editor.apply();
 
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             //start sign in/sign up activity
@@ -86,18 +104,67 @@ public class ChatRoom extends AppCompatActivity {
             @Override
             protected void populateView(View v, ChatMessage model, int position) {
                 //Get references to the view of message
-                TextView messageText = (TextView) v.findViewById(R.id.message_text);
+                chtbx = (RelativeLayout) v.findViewById(R.id.chat_box);
+                final TextView messageText = (TextView) v.findViewById(R.id.message_text);
                 TextView messageTime = (TextView) v.findViewById(R.id.message_time);
                 TextView messageUser = (TextView) v.findViewById(R.id.message_user);
 
+                String email = model.getMessageUser();
+                if (FirebaseAuth.getInstance().getCurrentUser() !=null && FirebaseAuth.getInstance().getCurrentUser().getEmail().toString().equals(email)) {
+                    chtbx.setBackground(getResources().getDrawable(R.drawable.chtbox_2));
+                } else {
+
+                }
+
                 //set values
-                messageText.setText(model.getMessageText());
+                //messageText.setText(model.getMessageText());
                 messageUser.setText(model.getMessageUser());
                 //messageTime.setText(java.text.DateFormat.getInstance().format(model.getMessageTime()));
                 messageTime.setText(model.getMessageTime());
 
                 //Format the time before showing it
                 //messageTime.setText(model.getMessageTime(), DateFormat.format("dd-MM-yyyy(HH:mm:ss)"));
+                String API_KEY = "trnsl.1.1.20180421T080208Z.a562b322b8b83091.82550c2bbb57afa2ac0b5442374a359774762fea";
+                String defaultUrl="https://translate.yandex.net/api/v1.5/tr.json/translate";
+
+                String language = getSharedPreferences("MyPrefs",MODE_PRIVATE).getString("language", "en");
+                String stringUrl=defaultUrl+"?key="+API_KEY+"&text="+model.getMessageText()+"&lang="+findLangCode(language.toUpperCase());
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, stringUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                try {
+                                    String OutputString;
+                                    //getting the whole json object from the response
+                                    JSONObject obj = new JSONObject(response);
+                                    JSONArray arr = obj.getJSONArray("text");
+                                    OutputString = arr.getString(0);
+                                    messageText.setText(OutputString);
+
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                //displaying the error in toast if occurs
+
+                                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+                                //TextView error= findViewById(R.id.error);
+                                //error.setText(e.getMessage());
+                            }
+                        });
+                //creating a request queue
+                RequestQueue requestQueue = Volley.newRequestQueue(ChatRoom.this);
+                //adding the string request to request queue
+                requestQueue.add(stringRequest);
+
             }
         };
         list.setAdapter(adapter);
@@ -108,12 +175,16 @@ public class ChatRoom extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 10) {
             if (resultCode == RESULT_OK) {
-                Toast.makeText(ChatRoom.this, "Welcome!!!" + FirebaseAuth.getInstance().getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(ChatRoom.this, "Welcome! " + FirebaseAuth.getInstance().getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
                 displayChatMessages();
             } else {
                 Toast.makeText(ChatRoom.this, "We couldn't sign you in..", Toast.LENGTH_SHORT).show();
                 finish();
             }
+        }
+        if(requestCode==2){
+            Log.d("Vaishali", "onActivityResult: request code 2");
+            displayChatMessages();
         }
     }
 
@@ -137,9 +208,234 @@ public class ChatRoom extends AppCompatActivity {
         }
         else if (item.getItemId() == R.id.select_language) {
             SelectLanguage cdd = new SelectLanguage(ChatRoom.this);
-            cdd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             cdd.show();
+            Window window = cdd.getWindow();
+            window.setLayout(WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+            cdd.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    displayChatMessages();
+                }
+            });
         }
         return true;
     }
+
+    public static String findLangCode(String lang){
+        String langShort = "en";
+
+        if(Objects.equals(lang, "AFRIKAANS")){
+            langShort="af";
+        }
+        if(Objects.equals(lang, "ALBANIAN")){
+            langShort="sq";
+        }
+        if(Objects.equals(lang, "AMHARIC")){
+            langShort="am";
+        }
+        if(Objects.equals(lang, "ARABIC")){
+            langShort="ar";
+        }
+        if(Objects.equals(lang, "ARMENIAN")){
+            langShort="hy";
+        }
+        if(Objects.equals(lang, "AZERBAIJANI")){
+            langShort="az";
+        }
+        if(Objects.equals(lang, "BASHKIR")){
+            langShort="ba";
+        }
+        if(Objects.equals(lang, "BASQUE")){
+            langShort="eu";
+        }
+        if(Objects.equals(lang, "BENGALI")){
+            langShort="bn";
+        }
+        if(Objects.equals(lang, "BELARUSIAN")){
+            langShort="be";
+        }
+        if(Objects.equals(lang, "BOSNINAN")){
+            langShort="bs";
+        }
+        if(Objects.equals(lang, "BULGARIAN")){
+            langShort="bg";
+        }
+        if(Objects.equals(lang, "CATALAN")){
+            langShort="ca";
+        }
+        if(Objects.equals(lang, "CHINESE")){
+            langShort="zh";
+        }
+        if(Objects.equals(lang, "CROATIAN")){
+            langShort="hr";
+        }
+        if(Objects.equals(lang, "CZECH")){
+            langShort="cs";
+        }
+        if(Objects.equals(lang, "DANISH")){
+            langShort="da";
+        }
+        if(Objects.equals(lang, "DUTCH")){
+            langShort="nl";
+        }
+        if(Objects.equals(lang, "ENGLISH")){
+            langShort="en";
+        }
+        if(Objects.equals(lang, "ESTONIAN")){
+            langShort="et";
+        }
+        if(Objects.equals(lang, "FINNISH")){
+            langShort="fi";
+        }
+        if(Objects.equals(lang, "FRENCH")){
+            langShort="fr";
+        }
+        if(Objects.equals(lang, "GALICIAN")){
+            langShort="gl";
+        }
+        if(Objects.equals(lang, "GEORGIAN")){
+            langShort="ka";
+        }
+        if(Objects.equals(lang, "GERMAN")){
+            langShort="de";
+        }
+        if(Objects.equals(lang, "GREEK")){
+            langShort="el";
+        }
+        if(Objects.equals(lang, "GUJARATI")){
+            langShort="gu";
+        }
+        if(Objects.equals(lang, "HAITIAN")){
+            langShort="ht";
+        }
+        if(Objects.equals(lang, "HEBREW")){
+            langShort="he";
+        }
+        if(Objects.equals(lang, "HINDI")){
+            langShort="hi";
+        }
+        if(Objects.equals(lang, "HUNGARIAN")){
+            langShort="hu";
+        }
+        if(Objects.equals(lang, "ICELANDIC")){
+            langShort="is";
+        }
+        if(Objects.equals(lang, "INDONESIAN")){
+            langShort="id";
+        }
+        if(Objects.equals(lang, "IRISH")){
+            langShort="ga";
+        }
+        if(Objects.equals(lang, "ITALIAN")){
+            langShort="it";
+        }
+        if(Objects.equals(lang, "JAPANESE")){
+            langShort="ja";
+        }
+        if(Objects.equals(lang, "JAVANESE")){
+            langShort="jv";
+        }
+        if(Objects.equals(lang, "KANNADA")){
+            langShort="kn";
+        }
+        if(Objects.equals(lang, "KOREAN")){
+            langShort="ko";
+        }
+        if(Objects.equals(lang, "LATIN")){
+            langShort="la";
+        }
+        if(Objects.equals(lang, "LATVIAN")){
+            langShort="lv";
+        }
+        if(Objects.equals(lang, "LITHUANIAN")){
+            langShort="lt";
+        }
+        if(Objects.equals(lang, "MACEDONIAN")){
+            langShort="mk";
+        }
+        if(Objects.equals(lang, "MALAY")){
+            langShort="ms";
+        }
+        if(Objects.equals(lang, "MALAYALAM")){
+            langShort="ml";
+        }
+        if(Objects.equals(lang, "MALTESE")){
+            langShort="mt";
+        }
+        if(Objects.equals(lang, "MARATHI")){
+            langShort="mr";
+        }
+        if(Objects.equals(lang, "NEPALI")){
+            langShort="ne";
+        }
+        if(Objects.equals(lang, "NORWEGIAN")){
+            langShort="no";
+        }
+        if(Objects.equals(lang, "PERSIAN")){
+            langShort="fa";
+        }
+        if(Objects.equals(lang, "POLISH")){
+            langShort="pl";
+        }
+        if(Objects.equals(lang, "PORTUGUESE")){
+            langShort="pt";
+        }
+        if(Objects.equals(lang, "PUNJABI")){
+            langShort="pu";
+        }
+        if(Objects.equals(lang, "ROMANIAN")){
+            langShort="ro";
+        }
+        if(Objects.equals(lang, "RUSSIAN")){
+            langShort="ru";
+        }
+        if(Objects.equals(lang, "SERBIAN")){
+            langShort="sr";
+        }
+        if(Objects.equals(lang, "SLOVAK")){
+            langShort="sk";
+        }
+        if(Objects.equals(lang, "SLOVENIAN")){
+            langShort="sl";
+        }
+        if(Objects.equals(lang, "SPANISH")){
+            langShort="es";
+        }
+        if(Objects.equals(lang, "SWAHILI")){
+            langShort="sw";
+        }
+        if(Objects.equals(lang, "SWEDISH")){
+            langShort="sv";
+        }
+        if(Objects.equals(lang, "TAMIL")){
+            langShort="ta";
+        }
+        if(Objects.equals(lang, "TELUGU")){
+            langShort="te";
+        }
+        if(Objects.equals(lang, "THAI")){
+            langShort="th";
+        }
+        if(Objects.equals(lang, "TURKISH")){
+            langShort="tr";
+        }
+        if(Objects.equals(lang, "UKRAINIAN")){
+            langShort="uk";
+        }
+        if(Objects.equals(lang, "URDU")){
+            langShort="ur";
+        }
+        if(Objects.equals(lang, "VIETNAMESE")){
+            langShort="vi";
+        }
+        if(Objects.equals(lang, "WELSH")){
+            langShort="cy";
+        }
+        if(Objects.equals(lang, "YIDDISH")){
+            langShort="yi";
+        }
+
+        return langShort;
+    }
+
 }
--- app/src/main/java/com/example/ramesh/chatapplication/SelectLanguage.java
+++ app/src/main/java/com/example/ramesh/chatapplication/SelectLanguage.java
@@ -1,18 +1,33 @@
 package com.example.ramesh.chatapplication;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by hp on 4/21/2018.
  */
 
 public class SelectLanguage extends Dialog {
+
+    Spinner spinnerLang;
+    SharedPreferences sharedPreferences;
+    Button ok;
+
+
     public SelectLanguage(@NonNull Context context) {
         super(context);
     }
@@ -22,5 +37,34 @@ public class SelectLanguage extends Dialog {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.select_language);
 
+        sharedPreferences = getContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        spinnerLang = (Spinner) findViewById(R.id.select_lang_spinner);
+        spinnerLang.setSelection(sharedPreferences.getInt("position", 0));
+        ok = (Button) findViewById(R.id.btn_ok);
+
+        spinnerLang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String lang = (String) parent.getItemAtPosition(position);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("language", lang);
+                editor.putInt("position",position);
+                editor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
     }
+
 }
